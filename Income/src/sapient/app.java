3c72b913de9e415e3ed798fc82a53ddeed5095a9
suchithra reddy <suchithranathi@gmail.com>
2020-08-27 11:43:59 +0530

    folder 1 added

---
+++ Income/src/sapient/app.java
@@ -0,0 +1,13 @@
+package sapient;
+import java.io.IOException;
+public class app {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		String csvFile = "input.csv";
+	       csvreader.read(csvFile);
+	       outputTableGenerator.printLog();;
+
+	}
+
+}
